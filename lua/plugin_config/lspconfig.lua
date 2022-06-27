7ed351bb7aabc389ea829d84f207e31855d9a32f
Dang Kai <dk@incus>
2022-06-27 16:01:51 +0800

    Merge LSP lua config into main LSP config file.

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -5,20 +5,68 @@ if not ret_ok then
     return
 end
 
--- Lua
-local opt = require("plugin_config.lsp.lua")
-inst.sumneko_lua.setup{
-    on_attach = opt.on_attach,
-    flags     = opt.flags,
-    settings  = opt.settings,
+-- Common settings
+local flags = {
+    debounce_text_changes = 150,
 }
 
+local on_attach = function(client, bufnr)
+    -- Disable code formatting
+    --client.resolved_capabilities.document_formatting = false
+    --client.resolved_capabilities.document_range_formatting = false
+
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    require('keybinding').lsp(buf_set_keymap)
+
+    -- Auto format when the file is saved.
+    --vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+end
+
+local settings = {}
+
+-- Lua
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+settings.Lua = {
+   runtime = {
+       -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+       version = 'LuaJIT',
+       -- Setup your lua path
+       path = runtime_path,
+   },
+   diagnostics = {
+       -- Get the language server to recognize the `vim` global
+       globals = { 'vim' },
+   },
+   workspace = {
+       -- Make the server aware of Neovim runtime files
+       library = vim.api.nvim_get_runtime_file('', true),
+       checkThirdParty = false,
+   },
+   -- Do not send telemetry data containing a randomized but unique identifier
+   telemetry = {
+       enable = false,
+   },
+}
+
+inst.sumneko_lua.setup({
+    on_attach = on_attach,
+    flags = flags,
+    settings = settings,
+})
+
 -- C/C++
-inst.clangd.setup{}
+inst.clangd.setup({
+    on_attach = on_attach,
+    flags = flags,
+})
 
 -- Python
-inst.pylsp.setup{}
+inst.pylsp.setup({})
 
 -- Latex
-inst.texlab.setup{}
-
+inst.texlab.setup({})
