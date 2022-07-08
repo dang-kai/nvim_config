e7ca2a8e799669322d57460d66755d1818c3f0fe
Dang Kai <dk@AEHost1.localdomain>
2022-07-08 17:00:21 +0800

    Change floaterm keybinding.

--- ftplugin/floaterm.lua
+++ ftplugin/floaterm.lua
@@ -2,10 +2,10 @@ local m = { noremap = true, silent = true }
 local map = vim.api.nvim_buf_set_keymap
 
 --map("t", "<F4>", "<C-\\><C-N>", m)
-map(0, "t", "<F1>", "<C-\\><C-N> :FloatermToggle<CR>", m) -- F1 is already escape terminal globally.
+map(0, "t", "<F1>", "<C-\\><C-N> :FloatermHide<CR>", m)
 map(0, "t", "<F2>", "<C-\\><C-N> :FloatermPrev<CR>", m)
 map(0, "t", "<F3>", "<C-\\><C-N> :FloatermNext<CR>", m)
-map(0, "t", "<F4>", "<C-\\><C-N> :FloatermHide<CR>", m)
+map(0, "t", "<F4>", "<C-\\><C-N> :FloatermKill<CR>", m)
 map(0, "t", "<C-T>", "<C-\\><C-N> :FloatermNew<CR>", m)
 map(0, "t", "<C-X>", "<C-\\><C-N> :FloatermKill<CR>", m)
 map(0, "t", "<C-Q>", "<C-\\><C-N>", m)
