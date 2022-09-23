394d80bb68d4cd954d7f711a54bf1916e92b77af
Dang Kai <dk@incus>
2022-09-23 09:57:53 +0800

    Update config.

--- lua/plugin_config/nvimtree.lua
+++ lua/plugin_config/nvimtree.lua
@@ -24,7 +24,7 @@ inst.setup({
         adaptive_size = true,
         centralize_selection = false,
         width = tree_width,
-        height = 30,
+        --height = 30,
         hide_root_folder = false,
         side = "left",
         preserve_window_proportions = false,
