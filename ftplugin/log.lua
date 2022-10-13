57a069ae795c92747c75358686ef40ed9bd91e94
Dang Kai <dk@AEHost1.localdomain>
2022-10-13 23:58:30 +0800

    Add more date insertion keymaps.

--- ftplugin/log.lua
+++ ftplugin/log.lua
@@ -2,3 +2,4 @@ local m = { noremap = true, silent = true }
 local map = vim.api.nvim_buf_set_keymap
 
 map(0, 'n', '<leader>id', ':r!date<CR>kJ$', m)
+map(0, 'n', '<leader>od', ':r!date<CR>o<ESC>o<ESC>k', m)
