9e7fb5127f06c4cf0155bf9033fa19e1dcccf264
Dang Kai <dk@incus>
2023-03-29 16:55:14 +0800

    Increase floaterm size.

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -166,8 +166,8 @@ local plugins = {
         lazy = true,
         event = 'VeryLazy',
         config = function()
-            vim.g.floaterm_width = 0.8
-            vim.g.floaterm_height = 0.8
+            vim.g.floaterm_width = 0.9
+            vim.g.floaterm_height = 0.9
         end,
     },
 
