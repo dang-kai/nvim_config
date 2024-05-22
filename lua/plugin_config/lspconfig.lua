b4ff81b4389e36a74a10c7130e36653d23954bd6
dk <dk@AEPort1>
2024-05-22 16:22:14 +0800

    Replace pyright with pylsp.

--- lua/plugin_config/lspconfig.lua
+++ lua/plugin_config/lspconfig.lua
@@ -43,7 +43,7 @@ local on_attach = function(client, bufnr)
     --map(bufnr, "n", "gh", "<cmd>Lspsaga hover_doc<CR>", m)
     --map(bufnr, "n", "gf", "<cmd>Lspsaga lsp_finder<CR>", m)
 
-    map(bufnr, 'n', 'gp', '<cmd>lua vim.diagnostic.open_float()<CR>', m)
+    map(bufnr, 'n', 'gf', '<cmd>lua vim.diagnostic.open_float()<CR>', m)
     map(bufnr, 'n', 'gk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', m)
     map(bufnr, 'n', 'gj', '<cmd>lua vim.diagnostic.goto_next()<CR>', m)
     --map(bufnr, "n", "th", ":ClangdSwitchSourceHeader<CR>", m) -- Toggle header.
@@ -68,32 +68,32 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-settings.Lua = {
-    runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        --path = runtime_path,
-    },
-    diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim', 'use' },
-    },
-    workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
-        checkThirdParty = false,
-    },
-    -- Do not send telemetry data containing a randomized but unique identifier
-    telemetry = {
-        enable = false,
-    },
-}
-
 inst.lua_ls.setup({
     on_attach = on_attach,
     flags = flags,
-    settings = settings,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                --path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim', 'use' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
 
 -- C/C++
@@ -103,10 +103,19 @@ inst.clangd.setup({
 })
 
 -- Python
-inst.pyright.setup({
+inst.pylsp.setup({
     on_attach = on_attach,
     flags = flags,
+    settings = { pylsp = { plugins = {
+        pyflakes = { enabled = true },
+        -- Configuration reference: https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+        pycodestyle = { ignore = { 'E501' } },
+    } } },
 })
+--inst.pyright.setup({
+--    on_attach = on_attach,
+--    flags = flags,
+--})
 
 -- Latex
 inst.texlab.setup({
